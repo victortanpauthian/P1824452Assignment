15e885ad5f15bc61d0904e8ae8b3c0ee0553189a
victortanpauthian <58180359+victortanpauthian@users.noreply.github.com>
2019-11-25 23:48:20 +0800

    asda

---
+++ app/src/main/java/sp/com/polytechnics.java
@@ -0,0 +1,15 @@
+package sp.com;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class polytechnics extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.polytechnics);
+    }
+}
