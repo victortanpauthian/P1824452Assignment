4236e8b6f7cde740f8e99532649607bf317fdee4
victortanpauthian <58180359+victortanpauthian@users.noreply.github.com>
2019-11-25 23:42:34 +0800

    asda

---
+++ app/src/main/java/sp/com/P1824452.java
@@ -0,0 +1,30 @@
+package sp.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class P1824452 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        /*new Handler().postDelayed(new Runnable() {adksnkadjlkjad
+            @Override
+            public void run() {
+                // Magic here
+            }
+        }, 3000); // Millisecond 1000 = 1 sec*/
+        //Intent myIntent = new Intent(getBaseContext(),   polytechnics.class);
+        //startActivity(myIntent);
+    }
+
+
+}
